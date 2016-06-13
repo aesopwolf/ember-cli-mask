4db84ab847eb32aa16b2a1cbab4417d7568e591c
luizcgr <luizcgr@gmail.com>
2016-06-13 16:28:30 -0300

    Remove duplicated didInsertElement

--- addon/components/input-mask.js
+++ addon/components/input-mask.js
@@ -50,6 +50,12 @@ export default Ember.Component.extend({
     if(!this.get('type')) {
       this.element.setAttribute('type', 'text');
     }
+    if (this.get('value')) {
+      var input = document.getElementById(this.id);
+      this.processValue(this.get('value').toString(), function(mask) {
+        input.value = mask.props.value;
+      });
+    }
   },
 
   validType() {
@@ -199,14 +205,5 @@ export default Ember.Component.extend({
 
   click(event) {
     this.onChange(event);
-  },
-
-  didInsertElement() {
-    if (this.get('value')) {
-      var input = document.getElementById(this.id);
-      this.processValue(this.get('value').toString(), function(mask) {
-        input.value = mask.props.value;
-      });
-    }
   }
 });
